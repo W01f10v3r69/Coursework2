66b1b0c4ca676a854832236c743ed1967e351c18
Shammah Ekong <73609694+W01f10v3r69@users.noreply.github.com>
2021-01-29 08:00:39 +0100

    Add files via upload

---
+++ cw/subjects.js
@@ -0,0 +1,92 @@
+let subjects = [
+    {
+        id: 1,
+        title: "Maths",
+        location: "London",
+        price: 10000,
+        image: "images/calc1.png",
+        spaces: 5,
+        quantity: 1
+    },
+    {
+        id: 2,
+        title: "Physics",
+        location: "Oxford",
+        price: 12000,
+        image: "images/calc1.png",
+        spaces: 5,
+        quantity: 1
+    },
+    {
+        id: 3,
+        title: "English",
+        location: "London",
+        price: 11000,
+        image: "images/calc1.png",
+        spaces: 5,
+        quantity: 1
+    },
+    {
+        id: 4,
+        title: "Chemistry",
+        location: "York",
+        price: 12000,
+        image: "images/calc1.png",
+        spaces: 5,
+        quantity: 1
+    },
+    {
+        id: 5,
+        title: "Music",
+        location: "Bristol",
+        price: 9000,
+        image: "images/calc1.png",
+        spaces: 5,
+        quantity: 1
+    },
+    {
+        id: 6,
+        title: "Computer Science",
+        location: "London",
+        price: 10000,
+        image: "images/calc1.png",
+        spaces: 5,
+        quantity: 1
+    },
+    {
+        id: 7,
+        title: "French",
+        location: "Oxford",
+        price: 10000,
+        image: "images/calc1.png",
+        spaces: 5,
+        quantity: 1
+    },
+    {
+        id: 8,
+        title: "Japanese",
+        location: "London",
+        price: 10500,
+        image: "images/calc1.png",
+        spaces: 5,
+        quantity: 1
+    },
+    {
+        id: 9,
+        title: "Spanish",
+        location: "York",
+        price: 8500,
+        image: "images/calc1.png",
+        spaces: 5,
+        quantity: 1
+    },
+    {
+        id: 10,
+        title: "Korean",
+        location: "Bristol",
+        price: 9500,
+        image: "images/calc1.png",
+        spaces: 5,
+        quantity: 1
+    }
+]
