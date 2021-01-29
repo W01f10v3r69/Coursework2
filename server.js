d60429d44d2c8e567dd6abfe284f8ba405c23086
Shammah Ekong <73609694+W01f10v3r69@users.noreply.github.com>
2021-01-29 03:09:52 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+
+const MongoClient = require('mongodb').MongoClient;
+
+let db;
+MongoClient.connect('mongodb+srv://wolflover:wolflover@nether.rl4hb.mongodb.net/cwdb?retryWrites=true&w=majority',
+(err, client) => {
+    db = client.db('cwdb')
+});
+
+app.use(express.json());
+
+app.param('collectionName', (req, res, next, collectionName) =>{
+    req.collection = db.collection(collectionName)
+    return next()
+});
+
+app.get('/', (req, res) =>{
+    res.send ('Select a collection, e.g., /collection/collectionName')
+});
+
+app.get('/collection/:collectionName', (req, res) => {
+    req.collection.find({}).toArray ((e, results) => {
+        if (e) return next(e)
+        res.send(results)
+    });
+});
+
+app.listen(3000);
+console.log ('server running on port 3000');
